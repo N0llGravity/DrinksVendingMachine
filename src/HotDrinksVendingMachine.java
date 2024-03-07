1930463fd90b1958e330553a0a0cadf6a81196b0
Artem <ar.toropov@mail.ru>
2024-03-08 01:06:09 +0300

    complete

--- src/HotDrinksVendingMachine.java
+++ src/HotDrinksVendingMachine.java
@@ -12,7 +12,7 @@ public class HotDrinksVendingMachine implements VendingMachine {
     @Override
     public void getProduct(double volume, float temperature, String name) {
         for (BottleDrink drink : listDrinks) {
-            if (drink.getName().equals(name) && drink.getVolume() == volume && ((HotDrink) drink).getTemperature() == temperature);
+            if (drink.getName().equals(name) && drink.getVolume() == volume && ((HotDrink) drink).getTemperature() == temperature)
                 System.out.println(drink);
         }
     }
